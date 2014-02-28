1dd9dfb5fae571fdea803341ab8e134562915a76
Trey <trey@Jeremys-MacBook-Air.local>
2014-02-28 10:50:12 -0600

    added Deque stubs

---
+++ One/Deque.java
@@ -0,0 +1,44 @@
+
+    /**
+     * @author trey
+     *
+     * @param <Item>
+     */
+
+    public class Deque<Item> implements Iterable<Item> {
+
+        // construct an empty deque
+        public Deque() {};               
+
+        // is the deque empty?
+        public boolean isEmpty() {
+            return false; 
+        }
+        // return the number of items on the deque
+        public int size() { 
+            return 1; 
+        }
+        // insert the item at the front
+        public void addFirst(Item item) {
+        } 
+        // insert the item at the end
+        public void addLast(Item item) {
+            
+        }
+        // delete and return the item at the front
+        public Item removeFirst()  {
+            Item i = new Item();
+            return i; 
+        }
+        // delete and return the item at the end
+        public Item removeLast() {
+            return Item; 
+        }
+        // return an iterator over items in order from front to end
+        public Iterator<Item> iterator() {
+            
+        } 
+
+        public static void main(String[] args) {
+        }
+     }
